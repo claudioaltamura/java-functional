848f795d2921b1958d5479bfc352d6eeca0e35e3
Claudio Altamura <claudio@claudioaltamura.de>
2021-07-14 12:08:29 +0200

    new: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![Build Status](https://travis-ci.com/claudioaltamura/java-functional.svg?branch=master)](https://travis-ci.com/claudioaltamura/java-functional)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # java-functional
 Functional Programming in Java
---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+  java
+  idea
+  eclipse
+	id ("com.github.ben-manes.versions") version "0.39.0"
+}
+
+repositories {
+  jcenter()
+}
+
+dependencies {
+
+	testImplementation("org.assertj:assertj-core:3.20.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+val test by tasks.getting(Test::class) {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+  }
+}
