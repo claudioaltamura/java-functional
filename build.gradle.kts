e3de5a2392738ae124b5ed33f567fc8f2b79c5da
Claudio Altamura <claudio@claudioaltamura.de>
2021-10-12 11:33:20 +0200

    chore: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,20 +4,21 @@ plugins {
   java
   idea
   eclipse
-	id ("com.github.ben-manes.versions") version "0.39.0"
+    id("com.diffplug.spotless") version "5.16.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 repositories {
-  jcenter()
+  mavenCentral()
 }
 
 dependencies {
 
-	testImplementation("org.assertj:assertj-core:3.20.2")
+	testImplementation("org.assertj:assertj-core:3.21.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 java {
